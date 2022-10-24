c1559d9ad119203df33f1e51f2ae0794051e21f2
huchanghao <2955568032@qq.com>
2022-10-24 21:10:59 +0800

    实现flat 和初始化节流

---
+++ 手写/debounce/ChDebonce.ts
@@ -0,0 +1,9 @@
+const ChDebounce = (fn:Function,delay=1000)=>{
+    let timer:number|null= null;
+    return ()=>{
+        if(timer) clearTimeout(timer)
+        setTimeout(() => {
+            fn()
+        }, delay);
+    }
+}
